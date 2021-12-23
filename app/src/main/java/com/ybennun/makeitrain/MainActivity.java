ab3dcab80ed3ee2112534eeb9b85df3bd5b7c7f7
YeshayahuBennun <ybennun@yahoo.com>
2021-12-23 21:49:44 +0200

    Commit#5 - Make It Rain - Adding Android SnackBar and Toast

--- app/src/main/java/com/ybennun/makeitrain/MainActivity.java
+++ app/src/main/java/com/ybennun/makeitrain/MainActivity.java
@@ -7,13 +7,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button makeItRain;
-    private Button showInfo;
+    //private Button makeItRain;
+    //private Button showInfo;
     private TextView moneyValue;
     private int moneyCounter = 0;
 
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        makeItRain = findViewById(R.id.buttonMakeItRain);
+        //makeItRain = findViewById(R.id.buttonMakeItRain);
         moneyValue = findViewById(R.id.moneyValue);
 
         //makeItRain.setOnClickListener(view -> Log.d("MainActivity","onClick: Make it rain!"));
@@ -35,5 +38,13 @@ public class MainActivity extends AppCompatActivity {
         moneyCounter += 1000;
         moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
         //Log.d("MIR", "onClick: " + moneyCounter);
+    }
+
+    public void showInfo(View view) {
+        //Toast.makeText(MainActivity.this, R.string.app_info, Toast.LENGTH_SHORT).show();
+
+        Snackbar.make(moneyValue, R.string.app_info, Snackbar.LENGTH_LONG).setAction("More", view1 -> {
+            Log.d("Snack", "ShowInfo: SnackBar More");
+        }).show();
     }
 }
